7357ec4719e62faa1887c41b711c057e0ef74834
Daren <darenk28@gmail.com>
2018-03-20 23:19:01 -0400

    Updated Csv/Database Loaders with functions

--- Gooser/src/loaders/CsvLoader.java
+++ Gooser/src/loaders/CsvLoader.java
@@ -1,18 +1,69 @@
 package loaders;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import nodes.Node;
 
 public class CsvLoader {
 	
+	static final String CSV_SEPARATOR = ",";
+	
 	String fileName;
 	List<Node> nodeList;
+	BufferedReader buffReader;
+	int nextNodeID = 1;
 	
 	public CsvLoader(String inFileName)
 	{
 		fileName = inFileName;
 		nodeList = new ArrayList<Node>();
+	}
+	
+	public void LoadData()
+	{
+		
+		String newLine = "";
+		String[] tempValueArray;
+		String[] nameRow;
+		int idx = 0;
+		Node tempNode;
+		String tempName = "";
+		String tempValue = "";
+		
+		try
+		{
+			buffReader = new BufferedReader(new FileReader(fileName));
+			nameRow = buffReader.readLine().split(CSV_SEPARATOR);
+			while ((newLine = buffReader.readLine()) != null)
+			{
+				tempValueArray = newLine.split(CSV_SEPARATOR);
+				tempNode = new Node(nextNodeID);
+				for(idx = 0; idx < nameRow.length; idx++)
+				{
+					tempName = nameRow[idx];
+					tempValue = tempValueArray[idx];
+					tempNode.Add(tempName, tempValue);
+				}
+				nodeList.add(tempNode);
+				nextNodeID++;
+			}
+		}
+		catch(FileNotFoundException fnfEx)
+		{
+			System.out.println(fnfEx.getMessage());
+		}
+		catch(IOException ioEx)
+		{
+			System.out.println(ioEx.getMessage());
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
 	}
 	
 	
--- Gooser/src/loaders/DatabaseLoader.java
+++ Gooser/src/loaders/DatabaseLoader.java
@@ -1,5 +1,54 @@
 package loaders;
 
-public class DatabaseLoader {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import nodes.Node;
+
+public class DatabaseLoader {
+	
+	//"org.gjt.mm.mysql.Driver"
+    String myDriver;
+    
+    //"jdbc:mysql://localhost/test"
+    String myUrl;
+    
+    List<Node> nodeList;
+    
+    public DatabaseLoader(String inDriver, String inUrl)
+    {
+    	myDriver = inDriver;
+    	myUrl = inUrl;
+    	nodeList = new ArrayList<Node>();
+    }
+    
+    public void LoadData(String inSqlQuery)
+    {
+    	try
+    	{
+	    	myDriver = "org.gjt.mm.mysql.Driver";
+	        myUrl = "jdbc:mysql://localhost/test";
+	        Class.forName(myDriver);
+	        Connection conn = DriverManager.getConnection(myUrl, "root", "");
+	        
+	        Statement st = conn.createStatement();
+	        ResultSet rs = st.executeQuery(inSqlQuery);
+	        
+	        while (rs.next())
+	        {
+	        	String s = rs.getString("test_data");
+	        }
+	        st.close();
+    	}
+    	catch(Exception e)
+    	{
+    		System.out.println(e.getMessage());
+    	}
+    	
+    }
+	
 }
